35acc51b6330e399743cb7f430f1fa6ae29d42b3
Dmytro <seaqqullprogramming@gmail.com>
2021-02-13 15:31:41 +0100

    Update: Point
     Description:
     - Gizmo visualization of the particular object

---
+++ Kool2PlayTest/Assets/Scripts/Utility/Points/ScenePoint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Utility.Points
+{
+    public abstract class ScenePoint : Base.BaseMonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] protected float _size = 0.3f;
+#pragma warning restore 0649
+
+        protected Color _gizmoDefault;
+
+        public abstract Color Color
+        {
+            get;
+        }
+
+
+        protected void OnDrawGizmos()
+        {
+            _gizmoDefault = Gizmos.color;
+
+            Gizmos.color = Color;
+            Gizmos.DrawWireSphere(transform.position, _size);
+
+            Gizmos.color = _gizmoDefault;
+        }
+    }
+}

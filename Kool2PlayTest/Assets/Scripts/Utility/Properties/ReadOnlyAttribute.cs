332a7acb61a12f75da0ef383a1169561da972dc5
Dmytro <seaqqullprogramming@gmail.com>
2021-02-13 16:54:12 +0100

    Update: ReadOnlyField & IRunLater
     Added:
     - ReadOnlyDrawer: restrict variable to be read-only in editor
     - IRunLater: Used to indicate that some class can delay execution of particular actions

---
+++ Kool2PlayTest/Assets/Scripts/Utility/Properties/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Utility.OwnAttribute
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+
+    public class BeginReadOnlyGroupAttribute : PropertyAttribute { }
+
+    public class EndReadOnlyGroupAttribute : PropertyAttribute { }
+}

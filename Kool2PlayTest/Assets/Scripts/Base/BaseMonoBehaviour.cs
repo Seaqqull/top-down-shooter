6ea79c2ba0ae1d1687802d8ad2ab0ab2521cec40
Dmytro <seaqqullprogramming@gmail.com>
2021-02-13 10:45:30 +0100

    Update: Base behaviour class
     Description:
     - Used to cache performance-sensitive data

---
+++ Kool2PlayTest/Assets/Scripts/Base/BaseMonoBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Utility.Base
+{
+    public class BaseMonoBehaviour : MonoBehaviour
+    {
+        public GameObject GameObj { get; private set; }
+        public Transform Transform { get; private set; }
+        public Quaternion Rotation
+        {
+            get { return Transform.rotation; }
+            set { Transform.rotation = value; }
+        }
+        public Vector3 Position 
+        {
+            get { return Transform.position; } 
+        }
+        public Vector3 Forward 
+        { 
+            get { return Transform.forward; } 
+        }
+
+
+        protected virtual void Awake()
+        {
+            Transform = transform;
+            GameObj = gameObject;
+        }
+    }
+}

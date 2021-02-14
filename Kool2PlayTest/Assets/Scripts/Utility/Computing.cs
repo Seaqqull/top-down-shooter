0b4c1a9ce564f7a16f9ffa293f0352bcfc77dc32
Dmytro <seaqqullprogramming@gmail.com>
2021-02-14 18:06:19 +0100

    Update: Computing
     Added:
     - Computing Data/Handler
    
     Description:
     - Gives encapsulation over a certain type of data with indicating of the handler that should be used to analyze the data.

---
+++ Kool2PlayTest/Assets/Scripts/Utility/Computing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Kool2Play.Utility
+{
+    public class ComputingHandler : ScriptableObject { }
+
+    public class ComputingData<T> : ScriptableObject
+        where T : struct
+    {
+        [SerializeField]
+        public T Data;
+    }
+
+    public class ComputingData<THandler, TData>
+        where THandler : ComputingHandler
+    {
+#pragma warning disable 0649
+        [SerializeField] private THandler _handler;
+        [SerializeField] private TData[] _data;
+
+        private IReadOnlyList<TData> _dataRestricted;
+#pragma warning restore 0649
+
+        public THandler Handler
+        {
+            get { return this._handler; }
+        }
+        public IReadOnlyList<TData> Data
+        {
+            get
+            {
+                return this._dataRestricted ??
+                    (this._dataRestricted = _data);
+            }
+        }
+
+    }
+}

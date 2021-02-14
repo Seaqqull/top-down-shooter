473653c69b9d63e026e9886d7d4e7fa4dd3ba496
Dmytro <seaqqullprogramming@gmail.com>
2021-02-14 22:14:28 +0100

    Update: Input
     Description:
     - InputController: used to notify subscribers about current [Movement] input state and [Mouse] position in game

---
+++ Kool2PlayTest/Assets/Scripts/Input/InputController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+using System;
+
+
+namespace Kool2Play.Utility.Input
+{
+    public class InputController : MonoBehaviour
+    {
+        [Serializable]
+        public class InputUpdateEvent : UnityEvent<Vector3> { }
+
+
+#pragma warning disable CS0649
+        [SerializeField] private InputUpdateEvent _onMovementUpdate;
+        [SerializeField] private InputUpdateEvent _onMouseUpdate;
+        [SerializeField] [Range(0.0f, 50.0f)] private float _maxDistance = 50.0f;
+#pragma warning restore CS0649
+
+        private Camera _camera;
+
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            _onMovementUpdate.Invoke(new Vector3(
+                UnityEngine.Input.GetAxis("Horizontal"),
+                0.0f,
+                UnityEngine.Input.GetAxis("Vertical"))
+            );
+
+            RaycastHit hit;
+            Ray ray = _camera.ScreenPointToRay(UnityEngine.Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, _maxDistance))
+            {
+                _onMouseUpdate.Invoke(hit.point);
+            }
+        }
+
+    }
+}

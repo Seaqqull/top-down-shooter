e75f0ba42f01b4bf004c01ba4c26fd6ed08e5651
Dmytro <seaqqullprogramming@gmail.com>
2021-02-14 19:00:38 +0100

    Update: Bullets & Shooting mode
     Added:
     - BulletRay
     - ImpactBullet
     - SimpleShootingMode
    
     Description:
     - BulletRay: Bullet that cheks collision through raycasting
     - ImpactBullet: Applies force on affected entity
     - SimpleShootingMode: Simple mode, that emits amount of bullets as was set in the editor

---
+++ Kool2PlayTest/Assets/Scripts/Weapon/Bullets/BulletRay.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+
+
+namespace Kool2Play.Weapons.Bullets
+{
+    public class BulletRay : ImpactBullet
+    {
+        [SerializeField] private float _projectileSpeedMultiplier;
+
+        private Vector3 _previousPosition;
+
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (Math.Abs(_projectileSpeedMultiplier) > float.Epsilon)
+            {
+                _speed *= _projectileSpeedMultiplier;
+                _rigidbody.AddForce(Transform.forward * (_speed * Time.fixedDeltaTime));
+            }
+
+            CheckCollision(_previousPosition);
+
+            _previousPosition = Transform.position;
+        }
+
+        protected override void OnTriggerEnter(Collider other) { }
+
+
+        protected override void InitStart()
+        {
+            base.InitStart();
+
+            _previousPosition = Transform.position;
+        }
+
+        private void CheckCollision(Vector3 position)
+        {
+            RaycastHit hit;
+            Vector3 direction = Transform.position - position;
+            Ray ray = new Ray(position, direction);
+            float dist = Vector3.Distance(Transform.position, position);
+
+            if (Physics.Raycast(ray, out hit, dist, _targetMask))
+            {
+                //Transform.position = hit.point;
+                Quaternion rot = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+                Vector3 pos = hit.point;
+
+
+                if ((_isLaunched) &&
+                    ((1 << hit.collider.gameObject.layer) & _targetMask) != 0)
+                {
+                    Behaviour.Entity affectedTarget = hit.collider.GetComponent<Behaviour.Entity>();
+
+                    if (affectedTarget)
+                    {
+                        OnBulletHit();
+                        OnTargetHit(affectedTarget);
+
+                        affectedTarget.Health.ModifyHealth(_damage);
+                    }
+                }
+
+                OnBulletDestroy(hit, true);
+            }
+        }
+    }
+}
---
+++ Kool2PlayTest/Assets/Scripts/Weapon/Bullets/ImpactBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Weapons.Bullets
+{
+    public abstract class ImpactBullet : ActiveBullet
+    {
+        [SerializeField] private float _impactPower;
+
+        public float ImpactPower
+        {
+            get { return this._impactPower; }
+        }
+
+        protected override void OnTargetHit(Behaviour.Entity target)
+        {
+            target.ApplyForce(Transform.forward * _impactPower);
+        }
+
+    }
+}

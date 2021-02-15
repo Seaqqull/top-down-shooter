4a9eb9d0a47cd77d724e4258dad63975d6e3efe1
Dmytro <seaqqullprogramming@gmail.com>
2021-02-15 21:18:33 +0100

    Update: Mouse click
     Added:
     - Mouse click for input controller

--- Kool2PlayTest/Assets/Scripts/Input/InputController.cs
+++ Kool2PlayTest/Assets/Scripts/Input/InputController.cs
@@ -14,6 +14,7 @@ namespace Kool2Play.Utility.Input
 #pragma warning disable CS0649
         [SerializeField] private InputUpdateEvent _onMovementUpdate;
         [SerializeField] private InputUpdateEvent _onMouseUpdate;
+        [SerializeField] private UnityEvent _onMouseClick;
         [SerializeField] [Range(0.0f, 50.0f)] private float _maxDistance = 50.0f;
 #pragma warning restore CS0649
 
@@ -27,17 +28,23 @@ namespace Kool2Play.Utility.Input
 
         private void Update()
         {
+            // Movement input
             _onMovementUpdate.Invoke(new Vector3(
                 UnityEngine.Input.GetAxis("Horizontal"),
                 0.0f,
                 UnityEngine.Input.GetAxis("Vertical"))
             );
 
+            // Mouse hover
             Ray ray = _camera.ScreenPointToRay(UnityEngine.Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance))
             {
                 _onMouseUpdate.Invoke(hit.point);
             }
+
+            // Mouse click
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+                _onMouseClick.Invoke();
         }
 
     }

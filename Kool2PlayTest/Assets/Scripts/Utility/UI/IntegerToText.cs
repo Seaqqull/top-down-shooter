271d9bfcd98b46a919415214374e3ef6f3a1fbb8
Dmytro <seaqqullprogramming@gmail.com>
2021-02-17 13:24:09 +0100

    Update: UI class
     Description
     - [IntegerToText]: Allows to show integer value via TextMeshPro

---
+++ Kool2PlayTest/Assets/Scripts/Utility/UI/IntegerToText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Utility.UI
+{
+    public class IntegerToText : MonoBehaviour
+    {
+        [SerializeField] private Variables.IntegerReference _value;
+        [SerializeField] private TMPro.TMP_Text _text;
+
+
+        public void UpdateText()
+        {
+            _text.text = _value.Value.ToString();
+        }
+    }
+}

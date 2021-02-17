eff28133d812a19e2dbc36ead65d92988eefd5de
Dmytro <seaqqullprogramming@gmail.com>
2021-02-17 13:22:12 +0100

    Update: Extention method
     Description:
     - [Map]: Allows to map value into another range

---
+++ Kool2PlayTest/Assets/Scripts/Utility/Extentions.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Kool2Play.Utility.Extentions
+{
+    public static class VectorOperations
+    {
+        public static float Map(float value, float istart, float istop, float ostart, float ostop)
+        {
+            return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+        }
+    }
+}

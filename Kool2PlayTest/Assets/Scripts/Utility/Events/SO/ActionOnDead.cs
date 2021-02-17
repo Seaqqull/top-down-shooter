886e83e387b54a18ce14e4b05541b97071e1031d
Dmytro <seaqqullprogramming@gmail.com>
2021-02-17 14:15:37 +0100

    Update: OnDead action
     Added:
     - ActionOnDead
     - OnDead SO
    
     Description:
     - [ActionOnDead]: Kills all enemies on the scene
     - [OnDeadSO]: SO object of that action

---
+++ Kool2PlayTest/Assets/Scripts/Utility/Events/SO/ActionOnDead.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+namespace Kool2Play.Utility.Events
+{
+    [CreateAssetMenu(menuName = "Action/OnDead")]
+    class ActionOnDead : EventSO
+    {
+        protected override void OnRaise()
+        {
+            var enemies = FindObjectsOfType<Behaviour.Enemies.Enemy>();
+
+            foreach (var enemy in enemies)
+            { 
+                enemy.Health.ModifyHealth(enemy.Health.Value);
+            }
+        }
+    }
+}
